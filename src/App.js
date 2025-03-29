72f0ece85ef0def82552a776f576f9f45ef4d04e
Walid Belegchour <93341586+WalidNuev9@users.noreply.github.com>
2025-03-29 04:11:56 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Link,Routes,Route} from 'react-router-dom';
+import React, { Component,useEffect,useState } from "react";
+import "../assets/App.css"
+import Home from './Home'
+import Contact from './Contact'
+import Portfolio from './Portfolio'
+import Blog from './Blog'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ 
 
-export default App;
+  
+  
+    class NotFound extends React.Component {
+      render() {
+        return (
+          <p>Page Non Trouvée</p>
+        );
+      }
+    }
+  
+    export default class Menu extends React.Component {
+      render() {
+        return (
+          <div>
+              <BrowserRouter>
+              <header>
+              <nav className="navbar navbar-expand navbar-dark bg-dark shadow fixed-top">
+              <div className="container-fluid text-danger">
+              <a className="navbar-brand fs-5 fw-bolder" href="#">Walid Belegchour</a>
+              <ul className="d-flex fs-5 navbar-nav ">
+                  <li className="nav-item">
+                      <Link className='items  nav-link' to="/">Home</Link>
+                  </li>
+                  <li className="nav-item">
+                      <Link className='items nav-link ' to="/portfolio">Portfolio</Link>
+                  </li>
+                  <li className="nav-item">
+                      <Link className='items nav-link ' to="blog">Blog</Link>
+                  </li>
+                  <li className='nav-item'>
+                      <Link className='items nav-link' to="/contact">Contact</Link>
+                  </li>
+              </ul>
+              </div>
+              </nav>
+              </header>
+              <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/portfolio" element={<Portfolio/>} />
+              <Route path="/blog" element={<Blog/>} />
+              <Route path="/contact" element={<Contact/>} />
+              <Route path="*" element={<NotFound/>} />
+              </Routes>
+              </BrowserRouter>
+          </div>
+        );
+      }
+    }  
